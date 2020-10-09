26474dfa1a47150bfd486bb5ab1c842ea8ffd22e
aiso90 <aws1994aws@gmail.com>
2020-10-09 14:44:59 +0100

    final master

--- src/main/java/come/demos/aws/multimodule/MainActivit.java
+++ src/main/java/come/demos/aws/multimodule/MainActivit.java
@@ -12,5 +12,7 @@ public class MainActivit extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("Hello World", "onCreate: branch 1 Simple log");
         Log.d("Hello World", "onCreate: branch 1 Simple log");
+        Log.d("Hello World", "onCreate: master always must be updated Simple log");
+        Log.d("Hello World", "this code belong to branch 2 part_ 2");
     }
 }
